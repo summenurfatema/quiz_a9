9de2f10ba4b5ae4a25d3da7034b3b3e6da2648fa
Fatema04 <shadrinmoni15@gmail.com>
2022-10-12 07:23:11 -0700

    home btn added

--- src/components/Rechart.js
+++ src/components/Rechart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 const Rechart = () => {
     const data = [
@@ -26,22 +26,23 @@ const Rechart = () => {
 
 
     return (
-        <div className='container mx-auto flex flex-col items-center'>
+        <div>
             <Nav></Nav>
-            <h1 className='text-4xl font-semibold text-gray-800 mt-5 border-b-2 border-gray-300 pb-5'>Chart of Total Quiz</h1>
-            <LineChart className='w-1/2 md:w-full mt-10'
-                width={600}
-                height={400}
-                data={data}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Line type="monotone" dataKey="Quiz" stroke="#82ca9d" />
-            </LineChart>
+            <div className='container mx-auto flex flex-col items-center'>
 
-
-        </div >
+                <h1 className='text-4xl font-semibold text-gray-800 mt-5 border-b-2 border-gray-300 pb-5'>Chart of Total Quiz</h1>
+                <LineChart className='w-1/2 md:w-full mt-10'
+                    width={600}
+                    height={400}
+                    data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="Quiz" stroke="#82ca9d" />
+                </LineChart>
+            </div >
+        </div>
     );
 };
 
