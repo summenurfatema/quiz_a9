04d4c337337bdfd8a73d186207b2e21ec7d58d95
Fatema04 <shadrinmoni15@gmail.com>
2022-10-12 06:48:37 -0700

    STATIC section added

--- src/components/Rechart.js
+++ src/components/Rechart.js
@@ -28,10 +28,10 @@ const Rechart = () => {
     return (
         <div className='container mx-auto flex flex-col items-center'>
             <Nav></Nav>
-
+            <h1 className='text-4xl font-semibold text-gray-800 mt-5 border-b-2 border-gray-300 pb-5'>Chart of Total Quiz</h1>
             <LineChart className='w-1/2 md:w-full mt-10'
                 width={600}
-                height={600}
+                height={400}
                 data={data}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
