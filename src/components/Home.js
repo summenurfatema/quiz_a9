0f244cd31e002f250665f7f4683a63f704f3be57
Fatema04 <shadrinmoni15@gmail.com>
2022-10-12 06:43:38 -0700

    home section added

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,8 @@ const Home = () => {
             <Nav></Nav>
             <div className='flex flex-col md:flex-col'>
                 <div className='flex flex-col items-center'>
-                    <img className='container mx-auto w-60' src='https://img.freepik.com/free-vector/questions-concept-illustration_114360-1513.jpg?w=360&t=st=1665570355~exp=1665570955~hmac=73db343e5856983c95baa01334bf62da06ede4548d46aa73038faa9264bb55e8' alt='home' />
-                    <h1 className=' w-[300px] text-4xl font-bold text-red-700  border-b-2 border-red-200 pb-6'>Select Your Favourite Category</h1>
+                    <img className='container mx-auto w-60 my-5' src='https://img.freepik.com/free-vector/questions-concept-illustration_114360-1513.jpg?w=360&t=st=1665570355~exp=1665570955~hmac=73db343e5856983c95baa01334bf62da06ede4548d46aa73038faa9264bb55e8' alt='home' />
+                    <h1 className=' w-1/2 text-4xl font-bold text-red-700  border-b-2 border-red-200 pb-6'>Select Your Favourite Category</h1>
 
                 </div>
                 <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 gap-5 my-10 p-10 items-center'>
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
@@ -8,23 +9,29 @@ const Option = ({ optn }) => {
     function check(e) {
 
         if (e.target.innerText === optn.correctAnswer) {
-            alert('right')
+            console.log(e.target.innerText)
+            toast('YAY ! You are right')
+
         }
         else {
-            alert('wrong')
+            toast('Ops ! You are wrong')
+
         }
+
     }
+    <ToastContainer />
+    const notify = () => toast(optn.correctAnswer)
     return (
         <div className='container mx-auto bg-gray-300 border-4 border-gray-300 space-y-4 my-4 py-4'>
 
             <div className='flex justify-center items-center'>
 
                 <h1 className='text-3xl font-semibold'>{optn.question.slice(3, -4)}</h1>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 float-right  ml-5">
+                <svg onClick={notify} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 float-right  ml-5">
                     <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
                     <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" />
                 </svg>
-
+                <ToastContainer />
             </div>
 
 
